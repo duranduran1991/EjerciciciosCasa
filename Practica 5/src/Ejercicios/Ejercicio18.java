933d9334eea1a1d9280726997de7b0c60eb21855
admin <admin@Win7_1TB>
2019-03-07 11:22:03 +0100

    Ejercicio 18,falta por terminarlo(cual es mayor de los numeros)

---
+++ Practica 5/src/Ejercicios/Ejercicio18.java
@@ -0,0 +1,17 @@
+package Ejercicios;
+import java.util.*;
+public class Ejercicio18 {
+	/*18. Programa que lee una secuencia 5 números y escribe cual ha sido el mayor.*/
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int numeros=0,i,j=1;
+		for(i=numeros;i<5;i++){
+			System.out.println(" Escribe el numero " + j +" : ");
+			numeros=sc.nextInt();
+			j++;
+			
+		}
+
+	}
+
+}
