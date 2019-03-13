3e5485a62bc19d557ff0e67d0d6f0701755dd3dd
duranduran1991 <jonaymarichalduran@gmail.com>
2019-03-13 12:32:06 +0100

    Practica 5 terminada

--- Practica5/src/Ejercicio20.java
+++ Practica5/src/Ejercicio20.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+
 public class Ejercicio20 {
-	/*20. Programa que muestra por pantalla los números del 1 al 100 sin mostrar aquellos
-	números que sean múltiplos de 5.*/
+	/*
+	 * 20. Programa que muestra por pantalla los números del 1 al 100 sin
+	 * mostrar aquellos números que sean múltiplos de 5.
+	 */
 	public static void main(String[] args) {
-		Scanner sc=new Scanner (System.in);
-		int num=0,i;
-		for (i=num;i<100; i +=5){
-			num =num + i;
+		Scanner sc = new Scanner(System.in);
+		int i=0;
+		for (i = 0; i < 100; i++) {
+			if (i % 5 != 0) {
+				System.out.println(" El numero a mostrar es el: " +i);
+			}
 		}
-		System.out.println(num);
+		System.out.println("Fin del programa");
 	}
-	
-	
+
 }
